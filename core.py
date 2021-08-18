a3a8b3adcd61837610300876d97b583c088a781d
bai_asuka <296010236@qq.com>
2021-08-18 13:34:43 +0800

    增加byte数据的拼接，支持更长数据的收发

--- core.py
+++ core.py
@@ -3,20 +3,26 @@ import json
 import time
 from threading import Thread
 
+# 消息比特字符终结标识
+MESSAGE_OVER_FLAG = b'2xx@oo$n'
 
 ip_list = {}
 
+
 def send_msg(cilent, addr):
     try:
+        bytes_queue = b''
         while True:
-            content = cilent.recv(1024)
-            if content:
-                print(json.loads(content.decode("utf-8")))
-                for cil in ip_list.values():
-                    cil.send(content)
+            new_bytes = cilent.recv(1024)
+            if new_bytes:
+                bytes_queue += new_bytes
+                byte_split = bytes_queue.split(MESSAGE_OVER_FLAG)
+                if len(byte_split) == 2:
+                    byte_content, bytes_queue = byte_split
+                    for cil in ip_list.values():
+                        cil.send(byte_content + MESSAGE_OVER_FLAG)
     except Exception as e:
         print(e)
-        cilent.close()
 
 class Core:
     def __init__(self):
@@ -37,6 +43,7 @@ class Core:
                 ip_list[addr] = cilent
         except Exception as e:
             print("server shutdown")
+
 
 if __name__ == '__main__':
     core = Core()
